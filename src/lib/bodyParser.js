5f442f3dede9a8ea85a5336cced07f709ed0706f
RubenZubicoa <rubenzubicoatic@gmail.com>
2020-04-20 14:41:57 +0200

    first commit

---
+++ src/lib/bodyParser.js
@@ -0,0 +1,18 @@
+function bodyParser(req) {
+    return new Promise((resolve, reject) => {
+        let totalData = '';
+        req.on('data', chunk => {
+            totalData += chunk;
+        })
+            .on('end', () => {
+                req.body = JSON.parse(totalData);
+                resolve();
+            })
+            .on('error', err => {
+                console.log(err);
+                reject();
+            })
+    })
+}
+
+module.exports = { bodyParser }
